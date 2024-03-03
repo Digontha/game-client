9bbb43236adfd4c12d5259111d0a9d5a354428cc
Digontha <digonthodj@gmail.com>
2024-03-03 14:48:41 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            Hello i am from Home
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
