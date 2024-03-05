bd32012381aba0e47ccbb49b0ddbc77a96ef6a88
Digontha <digonthodj@gmail.com>
2024-03-05 15:47:49 +0600

    implement search functionality

--- src/Pages/Home/Games/Games.jsx
+++ src/Pages/Home/Games/Games.jsx
@@ -8,30 +8,39 @@ const Games = () => {
     const [games, setGames] = useState()
     // console.log(games);
     const [pages, setPages] = useState(1)
+    const [title, setTitle] = useState("")
     console.log(pages);
     const Plus = () => {
         if (pages < 3) {
             setPages(pages + 1);
         }
     }
-    
+
     const Minus = () => {
         if (pages > 1) {
             setPages(pages - 1);
         }
     }
     useEffect(() => {
-        axios.get(`https://www.cheapshark.com/api/1.0/deals?storeID=${pages}`)
+        axios.get(`https://www.cheapshark.com/api/1.0/deals?storeID=${pages}&title=${title}`)
             .then(res => {
                 setGames(res.data)
             })
             .catch(err => {
                 console.log(err);
             })
-    }, [pages])
+    }, [pages,title])
 
     return (
         <div>
+            <div className=" flex justify-center gap-3 mt-5">
+                <input
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+                placeholder="Search Your Desire Game"
+                 className="w-[50%] bg-[#211C13] rounded-lg border border-[#1B8EF8] px-4 py-2 text-[#1B8EF8] ring-offset-2 duration-300 focus:outline-none focus:ring-2" type="text" />
+                <button className="w-36 h-12 border-2 border-sky-300 text-sky-800 font-black rounded-full hover:text-white duration-300 relative group"><span className="absolute w-12 group-hover:w-[88%] duration-300 flex group-hover:justify-start rounded-full inset-2 bg-sky-300 group-hover:bg-sky-500 group-hover:duration-500 -z-10"></span>Search</button>
+            </div>
             <div className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-1 px-5 mt-10">
                 {
                     games?.map(game => <GamesCard key={game.gameID} game={game}></GamesCard>)
--- src/Shared/Banner/Banner.jsx
+++ src/Shared/Banner/Banner.jsx
@@ -16,6 +16,7 @@ export const Banner = () => {
 
   return (
     <div className="h-[240px] w-full md:h-[470px] lg:h-[670px] relative overflow-hidden">
+        
         {/* arrow left */}
         <button onClick={prevSlider} className="absolute top-1/2 left-3 z-50 flex justify-center items-center bg-white rounded-full w-6 h-6 md:w-8 md:h-8">
           <svg className="w-4 h-4 md:w-6 md:h-6 icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#0095FF" d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"></path></g></svg>
