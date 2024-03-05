2aac2d3948eeb01a541e75571b8dfd2fef25b102
Digontha <digonthodj@gmail.com>
2024-03-05 15:21:58 +0600

    Implement pagination

--- src/Pages/Home/Games/Games.jsx
+++ src/Pages/Home/Games/Games.jsx
@@ -5,24 +5,44 @@ import GamesCard from "./GamesCard/GamesCard";
 
 const Games = () => {
 
-    const [games,setGames]=useState()
+    const [games, setGames] = useState()
     // console.log(games);
-
-    useEffect(()=>{
-        axios.get("https://www.cheapshark.com/api/1.0/deals?storeID=1")
-        .then(res=>{
-            setGames(res.data)
-        })
-        .catch(err=>{
-            console.log(err);
-        })
-    },[])
+    const [pages, setPages] = useState(1)
+    console.log(pages);
+    const Plus = () => {
+        if (pages < 3) {
+            setPages(pages + 1);
+        }
+    }
+    
+    const Minus = () => {
+        if (pages > 1) {
+            setPages(pages - 1);
+        }
+    }
+    useEffect(() => {
+        axios.get(`https://www.cheapshark.com/api/1.0/deals?storeID=${pages}`)
+            .then(res => {
+                setGames(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [pages])
 
     return (
         <div>
-            {
-                games?.map(game=><GamesCard key={game.gameID} game={game}></GamesCard>)
-            }
+            <div className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-1 px-5 mt-10">
+                {
+                    games?.map(game => <GamesCard key={game.gameID} game={game}></GamesCard>)
+                }
+            </div>
+
+            <div className="join grid grid-cols-2 text-white w-[30%] mx-auto my-10">
+                <button onClick={Minus} className="join-item btn btn-outline text-white ">Previous page</button>
+                <button onClick={Plus} className="join-item btn btn-outline text-white">Next</button>
+            </div>
+
         </div>
     );
 };
