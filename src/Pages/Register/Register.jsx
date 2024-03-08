dd581f35b38e1510311bb088f7cf462d7483df2b
Digontha <digonthodj@gmail.com>
2024-03-08 19:21:53 +0600

    implement authentication with firebase

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,85 @@
+import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { updateProfile } from "firebase/auth";
+import swal from "sweetalert";
 
 const Register = () => {
+
+    const { createUser, googleLogin } = useContext(AuthContext)
+    // const axiosPublic = useAxiosPublic()
+//   const navigate = useNavigate()
+  const handleRegister = (e) => {
+
+    e.preventDefault();
+    const name = e.target.name.value
+    const image = e.target.image.value
+    const email = e.target.email.value
+    const password = e.target.password.value
+    console.log(name, image, email, password);
+
+    if (!/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+={}\[\]:;"'<>,.?/\\|`])[A-Za-z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?/\\|`]{7,}$/.test(password)) {
+
+      swal("error", "password must be at least 6 characters one capital letter and one special letter", "error");
+    } else {
+      createUser(email, password, name, image)
+        .then(res => {
+          console.log(res.user);
+          swal("success", "Your account create successfully", "success");
+          updateProfile(res.user, { displayName: name, photoURL: image })
+            .then(() => {
+                console.log(res);
+            //   const userData = {
+            //     email,
+            //     name,
+            //     image,
+            //     subscribe: false,
+            //   }
+            //   axiosPublic.post("/users", userData)
+            //     .then(res => console.log(res.data))
+            //   navigate("/")
+            })
+          e.target.reset()
+        })
+        .catch(() => {
+
+          swal("error", "something went wrong", "error");
+        })
+    }
+
+
+
+
+  }
+  const handleGoogle = () => {
+    googleLogin()
+      .then((res) => {
+      console.log(res.data);
+        // const userData = {
+        //   email: res.user.email,
+        //   name: res.user.displayName,
+        //   image: res.user.photoURL,
+        //   subscribe: false
+
+        // }
+        // axiosPublic.post("/users", userData)
+        //   .then(res => console.log(res.data))
+        swal("success", "Your account create successfully", "success");
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("error", "something went wrong", "error");
+      })
+  }
+
     return (
         <>
             <div className='lg:flex items-center flex-row-reverse lg:w-full bg-[#171F13] lg:h-[100vh] text-white'>
             <div className='lg:w-1/2 flex flex-col justify-center items-center mt-10 '>
                     <h1 className="text-2xl font-semibold  ">Register</h1>
                     <div className="card lg:w-full">
-                        <form className="card-body">
+                        <form onSubmit={handleRegister} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
@@ -40,7 +111,7 @@ const Register = () => {
                         </form>
                         <Link className="text-center" to="/login"><p>Have a account? <span className="font-bold btn btn-link">login</span></p></Link>
                         <div className="p-5">
-                            <button className="btn btn-sm btn-neutral w-full"><FaGoogle></FaGoogle> Google</button>
+                            <button onClick={handleGoogle} className="btn btn-sm btn-neutral w-full"><FaGoogle></FaGoogle> Google</button>
                         </div>
                     </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {RouterProvider,} from "react-router-dom";
+import { RouterProvider, } from "react-router-dom";
 import './index.css'
 import router from './Router/Route';
+import AuthProvider from './AuthProvider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
