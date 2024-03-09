ba586941625fd30abfb3cc154d899ec79deecd23
Digontha <digonthodj@gmail.com>
2024-03-09 12:16:52 +0600

    implement user image on navbar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Navbar = () => {
+
+    const {user,logOut}=useContext(AuthContext)
+    const signOut=()=>{
+        logOut();
+    }
     const navLinks = <>
         <NavLink
             to="/"
@@ -53,11 +60,17 @@ const Navbar = () => {
                             {navLinks}
                         </ul>
                     </div>
+                   {user?
+                   <div className="navbar-end">
+                    <img className="w-10 h-10 rounded-full" src={user.photoURL} alt="" />
+                    <button onClick={signOut}>Log Out</button>
+                   </div>
+                   :
                     <div className="navbar-end">
 
                        <Link to="/login"> <button className="text-xl w-32 h-12 bg-black text-white relative overflow-hidden group z-10"><span className="absolute bg-white rotate-12 -inset-8 group-hover:duration-300 duration-700 scale-x-0 group-hover:scale-x-100 origin-left transform transition-transform"></span><span className="absolute bg-[#2E3D29] rotate-12 -inset-8 group-hover:duration-700 duration-500 scale-x-0 group-hover:scale-x-100 origin-left transform transition-transform"></span><span className="absolute bg-[#2B501C] rotate-12 -inset-8 group-hover:duration-500 duration-300 scale-x-0 group-hover:scale-x-50 origin-left transform transition-transform"></span><span className="absolute opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-700 ease-out text-center z-10 text-white">Login</span>Explore</button></Link>
 
-                    </div>
+                    </div>}
                 </div>
             </nav>
         </>
