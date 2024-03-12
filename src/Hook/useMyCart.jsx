42c7c1b777757a37d8f27a74179f768321e51e94
Digontha <digonthodj@gmail.com>
2024-03-12 21:29:04 +0600

    load my cart by email query

--- src/Hook/useMyCart.jsx
+++ src/Hook/useMyCart.jsx
@@ -1,13 +1,16 @@
 
+import { useContext } from 'react';
 import useAxiosPublic from '../Axios/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
+import { AuthContext } from '../AuthProvider/AuthProvider'
 
 const useMyCart = () => {
+    const {user} = useContext(AuthContext)
     const axiosPublic =useAxiosPublic();
     const {data: MyCart = [],isPending: loading,refetch} = useQuery({
         queryKey: ['MyCart'],
         queryFn: async() =>{
-            const res = await axiosPublic.get('/carts');
+            const res = await axiosPublic.get(`/carts?email=${user.email}`);
             return res.data;
         }
     })
