31ac43ca3c9397354ff47eca2946f837bd8dffe5
Digontha <digonthodj@gmail.com>
2024-03-24 00:04:06 +0600

    create useAllMyCart hook for getting all data of my cart

---
+++ src/Hook/useAllMyCart.jsx
@@ -0,0 +1,19 @@
+
+import useAxiosPublic from '../Axios/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
+
+const useAllMyCart = () => {
+    const axiosPublic =useAxiosPublic();
+    const {data: MyCart = [],isPending: loading,refetch} = useQuery({
+        queryKey: ['MyCart'],
+        queryFn: async() =>{
+            const res = await axiosPublic.get(`/carts`);
+            return res.data;
+        }
+    })
+
+     return [MyCart,loading,refetch]
+};
+
+
+export default useAllMyCart;
