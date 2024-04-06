9d38cd9774709cd0fed9afe00fc410f377f87544
Digontha <digonthodj@gmail.com>
2024-04-06 21:29:11 +0600

    create fotter for website

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                     <a className="link link-hover">Press kit</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
+                    <h6 className="footer-title">Legalfdghsd</h6>
                     <a className="link link-hover">Terms of use</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
